9bdab367b73cb1c26341daac4253d321f04f0058
Arun Ramachandran <arun.ramachandran6@wipro.com>
2020-08-20 11:28:43 +0100

    Updated unit test for utils & components

--- src/utils/weatherUtils.spec.js
+++ src/utils/weatherUtils.spec.js
@@ -1,4 +1,4 @@
-import { convertToFarenheit, convertToDegreeCelsius } from './weatherUtils';
+import { convertToFarenheit, convertToDegreeCelsius, roundOffdata, abstractTemperatureInfo } from './weatherUtils';
 
 describe('Test utility', () => {
     it('should convert a value in Degree to Farenheit', () => {
@@ -9,5 +9,28 @@ describe('Test utility', () => {
     it('it should convert a value in Farenheit to Degree', () => {
         expect(convertToDegreeCelsius(32)).toEqual(0)
         expect(convertToDegreeCelsius(50)).toEqual(10.0)
+    })
+
+    it('should round off to given position by executing the roundoff function', () => {
+        expect(roundOffdata(23.23, 10)).toEqual(23.2);
+    })
+
+    it('should abstract and revert back with the exected data to display weather widget', () => {
+        const spyData = {
+            main: {
+                temp: 10,
+                test: 'unwanted',
+                feels_like: 7,
+                temp_min: 5,
+                temp_max: 10
+            }
+        };
+
+        expect(abstractTemperatureInfo(spyData)).toEqual({
+            temp: spyData.main.temp,
+            feelsLike: spyData.main.feels_like,
+            tempMin: spyData.main.temp_min,
+            tempMax: spyData.main.temp_max
+        });
     })
 });
