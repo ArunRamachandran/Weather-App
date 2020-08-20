665093d0144c6c777a3b810ea14bbdde3d425b09
Arun Ramachandran <arun.ramachandran6@wipro.com>
2020-08-20 10:38:43 +0100

    Updated unit tests for React containers

--- src/containers/Actionbar.spec.js
+++ src/containers/Actionbar.spec.js
@@ -1,45 +1,31 @@
 import React from 'react';
-import { mount, shallow } from 'enzyme';
+import { mount } from 'enzyme';
+import { useDispatch, useSelector, Provider } from "react-redux";
 import configureStore from 'redux-mock-store'
+import * as Constants from '../constants/constants';
 import { updatePreferredConversion } from '../actions/actions';
 import { ActionBar } from './ActionBar';
 import { Toggle } from '../components/Toggle';
-import { Provider } from 'react-redux';
-import * as Constants from '../constants/constants';
 
-jest.mock('react-redux', () => ({
-    useSelector: jest.fn(fn => fn()),
-    Provider: ({ Children }) => Children
-}));
-
-const mockStore = configureStore(),
-
-initialState = {
-    weatherData: {
-        preferredConversion: Constants.CELSIUS,
-        weather: []
-    }
-},
-store = mockStore(initialState);
-
-const dispatch = jest.fn().mockImplementation(() => Promise.resolve());
-
-jest.mock('../actions/actions', () => ({
-    updatePreferredConversion: jest.fn()
+jest.mock('../components/Toggle', () => ({
+    Toggle: () => 'Toggle'
 }))
 
-jest.mock('../components/Toggle', () => 'Toggle');
+const mockStore = configureStore([]);
 
 describe('ActionBar', () => {
-    const wrapper = mount(
-        <Provider store={store}>
-            <ActionBar 
-                onChange={jest.fn()}
-                />
-        </Provider>
-    )
 
-    it('should run', () => {
+    const store = mockStore({
+        weatherData: {
+            preferredConversion: Constants.CELSIUS,
+            weather: []
+        }
+    })
+
+    const wrapper = mount(<Provider store={store}><ActionBar/></Provider>);
+
+    it('should render Actionbar component', () => {
+        expect(wrapper).toBeTruthy();
         expect(wrapper.find(Toggle)).toHaveLength(1);
     })
 });
